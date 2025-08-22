f15b033f9b25f1670518acdf7a39e8d4ab02f731
floataqq <floataqq@gmail.com>
2025-08-22 14:47:09 +0300

    more stuff from code review

--- src/main.c
+++ src/main.c
@@ -48,10 +48,9 @@ int read_equation_from_line(char *line, Equation *eq) {
 int read_equation_from_argv(Equation *eq, int argc, char *argv[]) {
   int result;
   
-  if (argc == 1)
-    return 1;
-  else if (argc != 4) {
-    printf("Error: invalid format in args!\n");
+  if (argc != 4) 
+  {
+    printf("Error: invalid format - not enough arguments!\n");
     return 1;
   }
 
@@ -59,48 +58,68 @@ int read_equation_from_argv(Equation *eq, int argc, char *argv[]) {
   sprintf(input, "%s %s %s", argv[1], argv[2], argv[3]);
   
   result = read_equation_from_line(input, eq);
+  if (result)
+    printf("Error: invalid format in args!\n");
   return result;
 }
 
-void compute_solutions(Equation *eq) {
+/* a == 0 case */
+void solve_linear_equation(Equation *eq) {
+  double a = eq->a, b = eq->b, c = eq->c;
+  int is_zero_b = is_zero(b);
+  int is_zero_c = is_zero(c);
+
+  if (!is_zero_b && is_zero_c) 
+  {
+    eq->tag = SINGLE;
+    eq->solutions[0] = 0;
+  } else if (!is_zero_c && is_zero_b) 
+  {
+    eq->tag = NONE;
+  } else if (is_zero_b && is_zero_c) 
+  {
+    eq->tag = INFINITE;
+  } else 
+  {
+    eq->tag = SINGLE;
+    eq->solutions[0] = -c / b;
+  }
+}
+
+/* a != 0 case */
+void solve_quadratic_equation(Equation *eq) {
   double a = eq->a, b = eq->b, c = eq->c;
   double d = b*b - 4 * a*c;
-  
-  // linear eqautions
-  if (is_zero(a)) {
-    int is_zero_b = is_zero(b);
-    int is_zero_c = is_zero(c);
-    if (!is_zero_b && is_zero_c) {
-      eq->tag = SINGLE;
-      eq->solutions[0] = 0;
-    } else if (!is_zero_c && is_zero_b) {
-      eq->tag = NONE;
-    } else if (is_zero_b && is_zero_c) {
-      eq->tag = INFINITE;
-    } else {
-      eq->tag = SINGLE;
-      eq->solutions[0] = -c / b;
-    }
-  } 
-  // quadratic equations
-  else if (is_zero(d)) {
+
+  if (is_zero(d)) 
+  {
     eq->tag = SINGLE;
     eq->solutions[0] = -b / (2 * a);
-  } else if (d > 0) {
+  } else if (d > 0) 
+  {
     double d_sqrt = sqrt(d);
     eq->tag = DOUBLE;
     eq->solutions[0] = (-b + d_sqrt) / (2 * a);
     eq->solutions[1] = (-b - d_sqrt) / (2 * a);
-  } else {
+  } else 
+  {
     eq->tag = NONE;
   }
+}
+
+void compute_solutions(Equation *eq) {
+  if (is_zero(eq->a))
+    solve_linear_equation(eq);
+  else 
+    solve_quadratic_equation(eq);
 }
 
 void print_solutions(Equation eq) {
   char eq_str[MAX_LENGTH];
   sprintf(eq_str, "%lg*x^2 + %lg*x + %lg = 0", eq.a, eq.b, eq.c);
 
-  switch (eq.tag) {
+  switch (eq.tag) 
+  {
     case NOT_COMPUTED:
       printf("Error: %s solutions were not yet computed!\n", eq_str);
       break;
@@ -123,23 +142,33 @@ void print_solutions(Equation eq) {
 
 int main(int argc, char *argv[]) {
   Equation equation;
+  
+  if (argc != 1) /* input from argv */ 
+  {
+    if (read_equation_from_argv(&equation, argc, argv))
+      return 1;
 
-  if (read_equation_from_argv(&equation, argc, argv)) {
+    compute_solutions(&equation);
+    print_solutions(equation);
+  } 
+  else /* input from stdin */ 
+  {
     int line_no = 0;
     char line[MAX_LENGTH];
 
     printf("Enter equation coefficients one by one on different lines:\n");
-        
 
-    while (1) {
+    while (1) 
+    {
       if(!fgets(line, MAX_LENGTH, stdin))
         break;
       int read_cnt = strlen(line);
-      if (read_cnt <= 1) // just \n or EOF
+      if (read_cnt <= 1) // just \n or empty string
         break;
 
       line_no++;
-      if (read_equation_from_line(line, &equation)) {
+      if (read_equation_from_line(line, &equation)) 
+      {
         printf("Warning: could not read equation at line %d!\n", line_no);
         continue;
       }
@@ -147,10 +176,8 @@ int main(int argc, char *argv[]) {
       compute_solutions(&equation);
       print_solutions(equation);
     }
-  } else {
-    compute_solutions(&equation);
-    print_solutions(equation);
   }
+
   return 0;
 }
 
